6c08e80217979f50608f8a70d5f4cd2264e2b368
Jacccop <56170241+Jacccop@users.noreply.github.com>
2020-06-05 17:06:53 +0200

    zmena

--- includes/lfBuilders.php
+++
@@ -1,4 +0,0 @@
-<aside class="lfBuilders">
-    <h3>Chceš sa stať BUILDEROM?</h3>
-    <h2>Daj nám vedieť <span class="builderLink">na servery !</span></h2>
-<aside>
--- includes/socialSites.php
+++
@@ -1,6 +0,0 @@
-   
-        <aside id="social-sites">
-            <a href="" class="facebook"><i class="fab fa-facebook"></i></a>
-            <a href="" class="youtube"><i class="fab fa-youtube"></i></a>
-            <a href="" class="discord"><i class="fab fa-discord"></i></a>
-        </aside>
